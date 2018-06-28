870bfcd70fe53e44510e0100a12b74e38903645c
Evans Kipyegon <kipyegoevans0@gmail.com>
2018-06-28 11:09:18 +0300

    some change

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function convertCurrency(amount, fromCurrency, toCurrency, cb) {
 
 //uncomment to test
 
-convertCurrency(10, 'USD', 'PHP', function(err, amount) {
+convertCurrency(10, 'USD', 'PHP', (err, amount)=> {
   let p = document.getElementById('convert');
-  p.innerHTML += `${amount} now`;
-});
+  p.innerHTML += amount + 'Yes';
+})
